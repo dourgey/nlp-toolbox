2c0116ba4e33ace8fc58c0e3d585f1e48e58ec87
zed <dourgey@gmail.com>
2022-02-14 23:30:23 +0800

    remove bert_cnn test code

--- model/bert_cnn.py
+++ model/bert_cnn.py
@@ -2,7 +2,7 @@ import tqdm
 from transformers import BertModel, BertConfig, BertTokenizerFast
 import torch
 import torch.nn as nn
-from model_config import ModelConfig
+from .model_config import ModelConfig
 
 
 class BertCNNConfig(ModelConfig):
@@ -70,52 +70,4 @@ class DistillBertCNN(nn.Module):
         out = self.dropout(out)
         out = self.fc_cnn(out)
         return out
-
-
-if __name__ == '__main__':
-    from utils.data import NLPDataset, ClassificationBasePreProcessor
-    from transformers import BertTokenizer, DistilBertTokenizerFast
-    from torch.utils.data import DataLoader
-    from torch.optim import Adam
-    device = "cuda"
-
-    bert_vocab = r'D:\Works\codes\voice_quality\voice_quality_labeling\voice-quality-labeling-srv\target\classes\config_online\label_config\hbgprivatecall\house\bert_vocab.txt'
-    tokenizer = DistilBertTokenizerFast("../test/distilbert/vocab.txt")
-    train_file = "../test/dataset/ertai_train.json"
-    valid_file = "../test/dataset/ertai_dev.json"
-    test_file = "../test/dataset/ertai_test.json"
-
-    config = BertCNNConfig(3, None, 4, [3, 4, 5], from_pretrain="../test/distilbert/")
-    model = DistillBertCNN(config).to(device)
-    optimizer = Adam(model.parameters())
-    critiron = torch.nn.CrossEntropyLoss()
-
-    preprocessor = ClassificationBasePreProcessor({"0": 0, "1": 1, "2": 2}, 128, tokenizer)
-    train_set = NLPDataset(train_file, preprocessor, tokenizer)
-
-    def collate_fn(batch):
-        inputs = torch.tensor([x.inputs for x in batch], dtype=torch.int32)
-        input_mask = torch.tensor([x.input_mask for x in batch], dtype=torch.int32)
-        segment_ids = torch.tensor([x.segment_ids for x in batch], dtype=torch.int32)
-        targets = torch.tensor([x.target for x in batch])
-
-        return inputs, input_mask, segment_ids, targets#.unsqueeze(1)
-
-    train_loader = DataLoader(train_set, 32, True, collate_fn=collate_fn)
-
-    epoch = 20
-    loss_list = []
-    for epoch in tqdm.tqdm(range(epoch)):
-        for inputs, input_mask, segment_ids, targets in train_loader:
-            inputs = inputs.to(device)
-            input_mask = input_mask.to(device)
-            segment_ids = segment_ids.to(device)
-            targets = targets.to(device)
-            x = {"input_ids": inputs, "attention_mask": input_mask}
-            y_hat = model(x)
-            loss = critiron(y_hat, targets)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
 
