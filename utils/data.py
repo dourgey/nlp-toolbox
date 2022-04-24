f187e2cf365b75189b71082f9e477f36aa162925
zed <dourgey@gmail.com>
2022-04-25 02:33:33 +0800

    数据处理，数据集构建

--- utils/data.py
+++ utils/data.py
@@ -10,7 +10,7 @@ from utils.my_log import logger
 
 
 class DataEntity:
-    def __init__(self, inputs=None, target=None, **kwargs):
+    def __init__(self, inputs, target, **kwargs):
         """
         用于nlp-toolbox的数据实体定义，
 
@@ -25,7 +25,15 @@ class DataEntity:
             self.__dict__[key] = kwargs[key]
 
 
+    def to(self, device):
+        for key in self.__dict__:
+            self[key].to(device)
+
+
 class IPreProcessor(metaclass=ABCMeta):
+    """
+    数据预处理基类，用于将原始数据转换为DataEntity
+    """
     @abstractmethod
     def __init__(self, **kwargs):
         pass
@@ -43,22 +51,27 @@ class IPreProcessor(metaclass=ABCMeta):
 
 class ClassificationBasePreProcessor(IPreProcessor):
     """
-    基础文本分类Collator
+    基础文本分类PreProcessor
     由于要兼容之前的格式，注意对之前格式进行处理，例如__call__()函数的前两行
     """
     def __init__(self, label_list, max_seq_length, tokenizer):
         """
-
+        PreProcessor初始化
         :param label_list: dict of label discription like {}
         :param max_seq_length:
         :param tokenizer:
         """
         super(ClassificationBasePreProcessor, self).__init__()
-        self.label_list = label_list
+        self.label_list = {label_list[i]: i for i in range(len(label_list))}
         self.max_seq_length = max_seq_length
         self.tokenizer = tokenizer
 
     def __call__(self, data: List[SimpleNamespace]) -> List[DataEntity]:
+        """
+        将解析JSON得到的数据转换为DataEntity列表，按Berts的输入格式进行处理（inputs, input_mask, segment_ids）
+        :param data: 解析JSON得到的数据
+        :return: DataEntity列表，每条数据处理后为一个DataEntity
+        """
         data_entities = []
         for example in data:
             inputs = self.tokenizer.tokenize(example.inputs)
@@ -69,20 +82,38 @@ class ClassificationBasePreProcessor(IPreProcessor):
             input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
             input_mask = [1] * len(input_ids)
 
-            while len(input_ids) < self.max_seq_length:
-                input_ids.append(0)
-                input_mask.append(0)
-                segment_ids.append(0)
             target = 0 if "predict_label" == example.labels else self.label_list[example.labels]
             data_entities.append(
                 DataEntity(inputs=input_ids, target=target, input_mask=input_mask, segment_ids=segment_ids))
         return data_entities
 
+
+class NLPCollator:
+    """
+    各个任务的collator_fn, 用于DataLoader的collate_fn，将DataEntity列表按需转换为torch.Tensor
+    """
     @staticmethod
-    def collator(batch_data):
+    def classificationBaseCollateFn(batch):
+        """
+        基础文本分类的collate_fn
+        :param batch: list of DataEntity
+        :return: tuple: (inputs, input_mask, segment_ids), target
+        """
+        maxlen = max([len(x.inputs) for x in batch])
+        inputs = []
+        input_mask = []
+        segment_ids = []
+        y = []
+        for x in batch:
+            inputs.append(torch.tensor(x.inputs + [0] * (maxlen - len(x.inputs))))
+            input_mask.append(torch.tensor(x.input_mask + [0] * (maxlen - len(x.input_mask))))
+            segment_ids.append(torch.tensor(x.segment_ids + [0] * (maxlen - len(x.segment_ids))))
+            y.append(x.target)
+        return (torch.stack(inputs), torch.stack(input_mask), torch.stack(segment_ids)), torch.tensor(y)
+
+    @staticmethod
+    def LabelingBaseCollateFn(batch):
         pass
-
-
 
 
 class NLPDataset(Dataset):
@@ -90,15 +121,11 @@ class NLPDataset(Dataset):
         """
         用于nlp-toolbox的数据集类
 
-        :param input_file: 输入文件，JSON格式 / 旧格式自动识别
+        :param input_file: 输入文件，JSON格式
         :param preprocessor: IPreProcessor子类，负责数据预处理
         :param batch_size: 批大小
         :param tokenizer: tokenizer, convert token to id
-        :param re_convert_tfrecord: 是否转换为tfrecord格式
-        :param out_file: tfrecord格式存储文件，re_covert_tfrecord=True时，必须指定该路径
-        :param shuffle_buffer_size:
         """
-        self.is_json = True
         self.preprocessor = preprocessor
         self.tokenizer = tokenizer
         data = self._read_json(input_file)
@@ -111,15 +138,6 @@ class NLPDataset(Dataset):
             line_list = []
             for line in fp:
                 line_list.append(json_to_obj(line))
-            return line_list
-
-    @classmethod
-    def _read_txt(cls, input_file):
-        with open(input_file, 'r', encoding='utf8') as fp:
-            line_list = []
-            for line in fp:
-                tokens = line.strip().split("[SEP]")
-                line_list.append(tokens)
             return line_list
 
 
@@ -141,43 +159,14 @@ class NLPDataset(Dataset):
 
 
 if __name__ == '__main__':
-    # tokenizer = tokenization.FullTokenizer(
-    #     vocab_file=r"D:\Works\codes\wubanlp_dev\data\demo\bert_vocab.txt", do_lower_case=False)
-    # rc = ClassificationWithRoleCollator(label_list=['__label__right', '__label__wrong'], name_to_features=name_to_features, tokenizer=tokenizer, max_seq_length=512)
+    from transformers import BertTokenizer
+    train_file = '../test/iPhone/train.json'
+    tokenizer = BertTokenizer.from_pretrained('../test/distilbert')
+    preprocessor = ClassificationBasePreProcessor(label_list=["0", "1", "2"], max_seq_length=128, tokenizer=tokenizer)
 
-    # for (input_ids, role_ids, input_mask, segment_ids, _), target in dataset:
-    #     print("inputs:", input_ids,
-    #            "\nrole_ids:", role_ids,
-    #            "\ninput_mask:", input_mask,
-    #            "\nsegment_ids:", segment_ids,
-    #            "\ntarget:", _,
-    #           "\ntarget:", target)
+    train_set = NLPDataset(train_file, preprocessor, tokenizer)
+    dataloader = DataLoader(train_set, batch_size=2, collate_fn=NLPCollator.classificationBaseCollateFn)
 
-    txt_file = r"C:\Users\Zed\Downloads\test.txt"
-    json_file = r"D:\Works\codes\wubanlp_dev\data\demo\classification\test.json"
-    #
-    # tokenizer = tokenization.FullTokenizer(
-    #     vocab_file=r"D:\Works\codes\wubanlp_dev\data\demo\bert_vocab.txt", do_lower_case=False)
-    # rc = ClassificationBaseCollator(label_list=['__label__right', '__label__wrong'], tokenizer=tokenizer, max_seq_length=512)
-    # dataset = Dataset(json_file, rc, 32, True, False, out_file=r"D:\Works\codes\wubanlp_dev\data\demo\classification\test.tfrecord")
-    #
-    # for d in dataset:
-    #     (input_ids, input_mask, segment_ids, _), target = d
-    #     print("inputs:", input_ids,
-    #            "\ninput_mask:", input_mask,
-    #            "\nsegment_ids:", segment_ids,
-    #            "\ntarget:", _,
-    #           "\ntarget:", target)
-    #     break
-
-
-
-    from transformers import BertTokenizer, BertModel, BertConfig
-    bert_vocab = r'D:\Works\codes\voice_quality\voice_quality_labeling\voice-quality-labeling-srv\target\classes\config_online\label_config\hbgprivatecall\house\bert_vocab.txt'
-    tokenizer = BertTokenizer(bert_vocab)
-    print(tokenizer(["测试", "测试2"], padding=True))
-
-    inputs = {'input_ids': torch.tensor([[101, 3844, 6407, 102, 0], [101, 3844, 6407, 123, 102]]), 'token_type_ids': torch.tensor([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]), 'attention_mask': torch.tensor([[1, 1, 1, 1, 0], [1, 1, 1, 1, 1]])}
-    config = BertConfig()
-    bert = BertModel(config)
-    print(bert(inputs))
+    for X, y in dataloader:
+        print(X, y)
+        break
