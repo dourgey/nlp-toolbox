a3eb6172a028016664c44f296ec525a0975eaf47
zed <dourgey@gmail.com>
2022-09-16 03:06:13 +0800

    trainer 添加混合精度训练

--- common/module/trainer.py
+++ common/module/trainer.py
@@ -2,6 +2,10 @@ import torch
 import tqdm
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torch.cuda.amp import autocast as autocast, GradScaler
+
+from model.textcnn import TextCNNConfig, TextCNN
+from utils.data import NLPDataset, ClassificationBasePreProcessor, NLPCollator
 
 
 class EARLY_STOP_STATUS:
@@ -37,8 +41,8 @@ class EarlyStopping:
 
 class Trainer:
     def __init__(self, train_loader: DataLoader, valid_loader: DataLoader, model: nn.Module,
-                 optimizer: torch.optim.Optimizer, criterion, num_epoch,
-                 test_loader, ckpt_path, early_stop=False, early_stop_patience=7, evaluate_fn=None, device='cpu', **kwargs):
+                 optimizer: torch.optim.Optimizer, criterion,
+                 test_loader, ckpt_path, early_stop=False, early_stop_patience=7, evaluate_fn=None, device='cpu', auto_mixed_precision=True, **kwargs):
         self.train_loader = train_loader
         self.valid_loader = valid_loader
         self.model = model
@@ -46,57 +50,102 @@ class Trainer:
         self.criterion = criterion
         self.test_loader = test_loader
         self.ckpt_path = ckpt_path
-        self.num_epoch = num_epoch
         self.early_stop = early_stop
         self.device = device
         self.total_step = 0
+        self.epoch_num = 0
         self.model.to(device)
         self.evaluate_fn = evaluate_fn
+        self.auto_mixed_precision = auto_mixed_precision
 
         self.early_stop = early_stop
         if early_stop:
             self.es = EarlyStopping(early_stop_patience)
 
-    def train(self):
+    def train(self, epoch_num):
         self.model.train()
-        for epoch in tqdm.tqdm(range(self.num_epoch)):
-            for x, y in self.train_loader:
+        scaler = GradScaler()
+        for epoch in tqdm.tqdm(range(epoch_num)):
+            self.epoch_num += 1
+            torch.cuda.empty_cache()   # 清理GPU缓存
+            for x, y in tqdm.tqdm(self.train_loader):
                 self.total_step += 1
-                x = x.to(self.device)
+                x = [_x.to(self.device) for _x in x]
                 y = y.to(self.device)
 
                 # forward
-                y_hat = self.model(x)
-                loss = self.criterion(y, y_hat)
+                if self.auto_mixed_precision and self.device == 'cuda':
+                    with autocast():
+                        y_pred = self.model(x)
+                        loss = self.criterion(y_pred, y)
 
-                # backward
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+                    # Backward and optimize
+                    # Scales loss. 为了梯度放大.
+                    scaler.scale(loss).backward()
+                    scaler.step(self.optimizer)
+                    scaler.update()
+
+                else:
+                    y_hat = self.model(x)
+                    loss = self.criterion(y_hat, y)
+
+                    # Backward and optimize
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
 
             if self.early_stop:
                 with torch.no_grad():
-                    val_loss = self.evaluate_fn(self.model, self.valid_loader)
+                    if self.evaluate_fn is None:
+                        val_loss = torch.mean(torch.tensor([self.criterion(self.model([_x.to(self.device) for _x in x]), y.to(self.device)) for x, y in self.valid_loader])).cpu()
+                    else:
+                        val_loss = self.evaluate_fn(self.model, self.valid_loader)
                     early_stop_status = self.es(val_loss)
                     if early_stop_status == EARLY_STOP_STATUS.NEED_SAVE_MODEL:
-                        self._save_cpkt(epoch, loss)
+                        self._save_cpkt()
                     elif early_stop_status == EARLY_STOP_STATUS.NEED_EARLY_STOP:
                         print("======= EARLY STOPPING  ========")
                         break
                     else:
                         continue
 
-    def _save_cpkt(self, epoch, loss):
+    def _save_cpkt(self):
+        """
+        保存检查点 save checkpoint
+        :return: None
+        """
         torch.save({
-            'epoch': epoch,
+            'epoch': self.epoch_num,
+            'step': self.total_step,
             'model_state_dict': self.model.state_dict(),
             'optimizer_state_dict': self.optimizer.state_dict(),
-            'loss': loss,
         }, self.ckpt_path)
 
     def _load_ckpt(self):
+        """
+        加载检查点 load checkpoint
+        :return: None
+        """
         checkpoint = torch.load(self.ckpt_path)
         self.model.load_state_dict(checkpoint['model_state_dict']).to(self.device)
         self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        epoch = checkpoint['epoch']
-        loss = checkpoint['loss']
+        self.epoch_num = checkpoint['epoch']
+        self.total_step = checkpoint['step']
+
+
+if __name__ == '__main__':
+    from transformers import BertTokenizer
+
+    train_file = '../test/iPhone/train.json'
+    tokenizer = BertTokenizer.from_pretrained('../../test/bert_www')
+    preprocessor = ClassificationBasePreProcessor(label_list=["0", "1", "2"], max_seq_length=128, tokenizer=tokenizer)
+
+    train_set = NLPDataset('../../test/iPhone/train.json', preprocessor, tokenizer)
+    valid_set = NLPDataset('../../test/iPhone/dev.json', preprocessor, tokenizer)
+    train_loader = DataLoader(train_set, batch_size=64, collate_fn=NLPCollator.classificationBaseCollateFn, shuffle=True)
+    valid_loader = DataLoader(valid_set, batch_size=64, collate_fn=NLPCollator.classificationBaseCollateFn, shuffle=False)
+    config = TextCNNConfig.from_config_file('../configs/textcnn.conf')
+    print(config)
+    model = TextCNN(config)
+    trainer = Trainer(train_loader, valid_loader, model, optimizer=torch.optim.Adam(model.parameters(), lr=1e-3), criterion=nn.CrossEntropyLoss(), test_loader=None, ckpt_path='../../test/ckpt/textcnn.ckpt', device='cuda', early_stop=True)
+    trainer.train(epoch_num=100)
